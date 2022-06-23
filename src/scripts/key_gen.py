c7a5f2aca0661aeaafea4675962e64b31c82f15e
luca-castellazzi <luca1.castellazzi@mail.polimi.it>
2022-06-23 17:59:11 +0200

    small fixes

--- src/scripts/key_gen.py
+++ src/scripts/key_gen.py
@@ -2,32 +2,36 @@
 
 import random
 
-random.seed(24)
+def main():
+    random.seed(24)
 
-l1 = []
-l2 = []
-l3 = []
+    l1 = []
+    l2 = []
+    l3 = []
 
-for _ in range(16):
-    r1 = hex(random.randint(0, 255)).replace('0x', '')
-    r2 = hex(random.randint(0, 255)).replace('0x', '')
-    r3 = hex(random.randint(0, 255)).replace('0x', '')
+    for _ in range(16):
+        r1 = hex(random.randint(0, 255)).replace('0x', '')
+        r2 = hex(random.randint(0, 255)).replace('0x', '')
+        r3 = hex(random.randint(0, 255)).replace('0x', '')
     
-    if len(r1) == 1:
-        r1 = f'0{r1}'
-    if len(r2) == 1:
-        r2 = f'0{r2}'
-    if len(r3) == 1:
-        r3 = f'0{r3}'
+        if len(r1) == 1:
+            r1 = f'0{r1}'
+        if len(r2) == 1:
+            r2 = f'0{r2}'
+        if len(r3) == 1:
+            r3 = f'0{r3}'
 
-    l1.append(r1)
-    l2.append(r2)
-    l3.append(r3)
+        l1.append(r1)
+        l2.append(r2)
+        l3.append(r3)
 
-k1 = ''.join(l1)
-k2 = ''.join(l2)
-k3 = ''.join(l3)
+    k1 = ''.join(l1)
+    k2 = ''.join(l2)
+    k3 = ''.join(l3)
 
-print(f'k1 = {k1}')
-print(f'k2 = {k2}')
-print(f'k3 = {k3}')
+    print(f'k1 = {k1}')
+    print(f'k2 = {k2}')
+    print(f'k3 = {k3}')
+
+if __name__ == '__main__':
+    main()
