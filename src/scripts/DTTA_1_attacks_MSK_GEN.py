85712319ff59d3ac20eda316a679aa274b3c330c
luca-castellazzi <luca1.castellazzi@mail.polimi.it>
2023-03-13 10:13:53 +0100

    DTTA_1 msk with and without generators

--- src/scripts/DTTA_1_attacks_MSK_GEN.py
+++ src/scripts/DTTA_1_attacks_MSK_GEN.py
@@ -5,8 +5,7 @@ import numpy as np
 from tqdm import tqdm
 from tensorflow.keras.models import load_model
 from tensorflow.keras.backend import clear_session
-from sklearn.preprocessing import StandardScaler
-# from tensorflow.keras.metrics import TopKCategoricalAccuracy
+from tensorflow.keras.metrics import TopKCategoricalAccuracy
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
 
 # Custom
@@ -14,7 +13,9 @@ import sys
 sys.path.insert(0, '../utils')
 import constants
 import results
-from data_loader import DataLoader, SplitDataLoader
+from batch_scalers import BatchStandardScaler
+from data_generator import DataGenerator
+from data_loader import DataLoader
 sys.path.insert(0, '../modeling')
 from models import msk_mlp
 
@@ -25,9 +26,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' # 1 for INFO, 2 for INFO & WARNINGs, 3 
 BYTE = 5
 TARGET = 'SBOX_OUT'
 EPOCHS = 100
-ADD_TRACES = 50000
-TOT = range(50000, 400000, ADD_TRACES) # 50k, 100k, 150k, 200k, 250k, 300k, 350k
+TOT = range(50000, 400000, 50000) # 50k, 100k, 150k, 200k, 250k, 300k, 350k
 VAL_TOT = 5000
+TOT_TEST = 50000
 
 TR_ORIGINAL_LEN = 8736
 STOP_SAMPLE = 7700
@@ -52,7 +53,9 @@ def main():
     # Definition of constant root path 
     RES_ROOT = f'{constants.RESULTS_PATH}/DTTA/msk/{n_devs}d'
 
-    for i, tot in enumerate(TOT):
+    for tot in TOT:
+
+        train_tot = tot - VAL_TOT
 
         ges = []
 
@@ -70,78 +73,86 @@ def main():
                 os.mkdir(TRACES_FOLDER)
             SAVED_MODEL_PATH = TRACES_FOLDER + f'/model_{"".join(train_devs)}vs{test_dev}.h5'
 
-            train_dl = SplitDataLoader(
-                train_files, 
-                tot_traces=ADD_TRACES,
-                train_size=1-(VAL_TOT/ADD_TRACES),
-                target=TARGET,
-                start_tr_idx=i,
-                byte_idx=BYTE,
+            batch_scaler = BatchStandardScaler(
+                tr_files=train_files,
+                tr_tot=train_tot,
+                tr_original_len=TR_ORIGINAL_LEN,
+                batch_size=hp['batch_size'],
                 stop_sample=STOP_SAMPLE
             )
-            train_data, val_data = train_dl.load()
-            x_train, y_train, _, _ = train_data 
-            x_val, y_val, _, _ = val_data # Val data is kept to consider callbacks
+            batch_scaler.fit()
 
-            # Scale data to 0-mean and 1-variance
-            scaler = StandardScaler()
-            scaler.fit(x_train)
-            x_train = scaler.transform(x_train) 
-            x_val = scaler.transform(x_val)   
+            train_indices = range(train_tot)
+            val_indices = range(train_tot, train_tot+VAL_TOT)
+
+            train_gen = DataGenerator(
+                tr_files=train_files,
+                tr_indices=train_indices,
+                target=TARGET,
+                byte_idx=BYTE,
+                scaler=batch_scaler,
+                batch_size=hp['batch_size'],
+                stop_sample=STOP_SAMPLE
+            )
+
+            val_gen = DataGenerator(
+                tr_files=train_files,
+                tr_indices=val_indices,
+                target=TARGET,
+                byte_idx=BYTE,
+                scaler=batch_scaler,
+                batch_size=hp['batch_size'],
+                stop_sample=STOP_SAMPLE
+            )
+
 
             clear_session() # Start with a new Keras session every time    
 
-                # # Train and Attack
-                # metrics = [
-                #     'accuracy',
-                #     TopKCategoricalAccuracy(k=10, name='topK')
-                # ] 
+            # Train and Attack
+            metrics = [
+                'accuracy',
+                TopKCategoricalAccuracy(k=10, name='topK')
+            ] 
 
-                # model = msk_mlp(
-                #     hp=hp, 
-                #     input_len=STOP_SAMPLE, 
-                #     n_classes=constants.N_CLASSES[TARGET],
-                #     metrics=metrics
-                # )
-            
-            # Continue the training only if the considered number of traces exceeds 50k
-            if tot > ADD_TRACES:
-                
-                DKTA_DIR = f'{constants.RESULTS_PATH}/DKTA/{TARGET}/byte{BYTE}/msk_{n_devs}d/{N_KEYS}k'
-                model = load_model(f'{DKTA_DIR}/model_{"".join(train_devs)}vs{test_dev}.h5')
-                    
-                callbacks = [
-                    EarlyStopping(
-                        monitor='val_loss', 
-                        patience=15
-                    ),
-                    ReduceLROnPlateau(
-                        monitor='val_loss',
-                        factor=0.2,
-                        patience=7,
-                        min_lr=1e-7),
-                    ModelCheckpoint(
-                        SAVED_MODEL_PATH,
-                        monitor='val_loss',
-                        save_best_only=True
-                    )
-                ]
-                
-                model.fit(
-                    x_train,
-                    y_train,
-                    validation_data=(x_val, y_val),
-                    epochs=EPOCHS,
-                    callbacks=callbacks,
-                    verbose=0
+            model = msk_mlp(
+                hp=hp, 
+                input_len=STOP_SAMPLE, 
+                n_classes=constants.N_CLASSES[TARGET],
+                metrics=metrics
+            )
+
+            #Training (with Validation)
+            callbacks = [
+                EarlyStopping(
+                    monitor='val_loss', 
+                    patience=15
+                ),
+                ReduceLROnPlateau(
+                    monitor='val_loss',
+                    factor=0.2,
+                    patience=7,
+                    min_lr=1e-7),
+                ModelCheckpoint(
+                    SAVED_MODEL_PATH,
+                    monitor='val_loss',
+                    save_best_only=True
                 )
+            ]
+            
+            model.fit(
+                train_gen,
+                validation_data=val_gen,
+                epochs=EPOCHS,
+                callbacks=callbacks,
+                verbose=0
+            )
 
             attack_model = load_model(SAVED_MODEL_PATH)  
 
             # Testing (every time consider random test traces from the same set)
             test_dl = DataLoader(
                 test_files, 
-                tot_traces=ADD_TRACES,
+                tot_traces=tot,
                 target=TARGET,
                 byte_idx=BYTE,
                 start_sample=0,
@@ -150,7 +161,7 @@ def main():
             x_test, _, pbs_test, tkb_test = test_dl.load()
 
             # Scale test data to 0-mean and 1-variance w.r.t. train data
-            x_test = scaler.transform(x_test)        
+            x_test = batch_scaler.transform(x_test)        
             
             # Compute GE
             ge = results.ge(
